b7c4df9288469a668e4fe60f3e290437fbf14fc1
Clutchnp <agarwalarnav2007@gmail.com>
2024-07-01 15:52:23 +0530

    better rust setup formatting works don't know why even though I haven't set it up?

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,21 @@
 --servers with default setups
+local on_attach = function(_, bufnr)
+    return {buffer = bufnr}
+  end
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local def_servers = {
                 "lua_ls",
-                "rust_analyzer",
                 "pyright",
                 "bashls",
 }
 -- defining setup fn for tables with default setups
     local lspsetup = function(server)
         local lspconfig = require("lspconfig")
-        lspconfig[server].setup({})
+        lspconfig[server].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+
+  })
     end
 return {
    "neovim/nvim-lspconfig",
@@ -19,20 +26,34 @@ return {
 
     config = function()
         require("mason").setup()
-        local capabilities = require('cmp_nvim_lsp').default_capabilities()
         require("mason-lspconfig").setup({
             -- here I am merging tables with default and non default setups using unpack
 ---@diagnostic disable-next-line: deprecated
-                ensure_installed = {unpack(def_servers)}
+                ensure_installed = {unpack(def_servers),"rust_analyzer",}
         })
 
 --calling the setup fn 
         for _,server in ipairs(def_servers) do
         lspsetup(server)
-        capabilities = capabilities
         --lspconfig.lua_ls.setup({})
         --lspconfig.rust_analyzer.setup({})
         --lspconfig.bashls.setup({})
         end
+        local lspconfig = require("lspconfig")
+        local util = require "lspconfig/util"
+        lspconfig.rust_analyzer.setup({
+          on_attach = on_attach,
+          capabilities = capabilities,
+          filetypes = {"rust"},
+          root_dir = util.root_pattern("Cargo.toml"),
+          settings = {
+            ['rust_analyzer'] = {
+            cargo = {
+              allFeatures = true,
+          }
+        }
+
+      }
+    })
     end,
 }
