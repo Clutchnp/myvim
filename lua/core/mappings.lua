14feb20ab7dce2174b03d4aff86847b7c5597d6f
Clutchnp <agarwalarnav2007@gmail.com>
2024-07-01 15:30:47 +0530

    idk what?

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,12 +1,10 @@
 -- Set mapleader to space
-    vim.g.mapleader = " "
--- Define key mappings
+    vim.g.mapleader = " " -- Define key mappings
 vim.keymap.set('n', '<leader>m', vim.cmd.Ex)
 vim.keymap.set('n', 'fd', function() vim.cmd('Telescope git_files') end)
 vim.keymap.set('n', 'fe', function() vim.cmd('Telescope find_files') end)
 vim.keymap.set('n', 'ff', function() vim.cmd('Telescope live_grep') end)
-vim.keymap.set('n', 'fl', function() vim.cmd('Telescope buffers') end)
-vim.keymap.set('n', 'fh', function() vim.cmd('Telescope help_tags') end)
+vim.keymap.set('n', 'fl', function() vim.cmd('Telescope buffers') end) vim.keymap.set('n', 'fh', function() vim.cmd('Telescope help_tags') end)
 local augroup = vim.api.nvim_create_augroup
 local general = augroup('general', {})
 vim.api.nvim_create_autocmd('LspAttach', {
