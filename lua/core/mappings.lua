78ccca56601ccd639306ca396bb1a1dfd7318d05
Clutchnp <clutch1356@proton.me>
2024-09-21 18:41:45 +0530

    added somethings

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -15,10 +15,20 @@ vim.keymap.set({'n','x'}, 'J', 'j')
 
 local augroup = vim.api.nvim_create_augroup
 local general = augroup('general', {})
+
+local function close_qf_after_jump()
+  if vim.bo.filetype == 'qf' then
+    vim.cmd('cclose')
+  end
+end
+
+vim.api.nvim_create_autocmd("BufLeave", {
+  callback = close_qf_after_jump
+})
 vim.api.nvim_create_autocmd('LspAttach', {
   group = general,
   callback = function(e)
-    local opts = { buffer = e.buf }
+    local opts = { buffer = e.buf,nowait=true}
     vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set('n', 'gl', function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set('n', 'gr', function() vim.lsp.buf.references() end, opts)
@@ -28,7 +38,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- I have mapped f13 to my capslock key 
     vim.keymap.set("n", '<F13>f', function() vim.lsp.buf.format() end, opts)
     vim.keymap.set('n', 's', 'ci')
+    vim.keymap.set('n', '<C-k>', ':wincmd k<CR>')
+    vim.keymap.set('n', '<C-j>', ':wincmd j<CR>')
+    vim.keymap.set('n', '<C-h>', ':wincmd h<CR>')
+    vim.keymap.set('n', '<C-l>', ':wincmd l<CR>')
+    vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
     vim.keymap.set('n', '<F13>', 'yi')
     vim.keymap.set({ "n", "v"}, "<leader>p", function() vim.cmd("PasteImage") end, opts)
+
   end
 })
