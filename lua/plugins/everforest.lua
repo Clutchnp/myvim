caaa29f08cacd87eb8d3bfa882602d3ad5878c2b
clutchnp <clutch1356@proton.me>
2025-11-12 22:41:40 +0530

    commit for some things

---
+++ lua/plugins/everforest.lua
@@ -0,0 +1,12 @@
+return {
+  "sainnhe/everforest",
+  name = "everforest",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.g.everforest_enable_italic = true
+    vim.g.everforest_transparent_background = 1
+    vim.g.everforest_disable_terminal_colors = 1
+    vim.cmd.colorscheme("everforest")
+  end,
+}
